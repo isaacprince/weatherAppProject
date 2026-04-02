fe934358cb3a4fd3d6e3d833680a7c8bfdf2e334
isaacprince <isaacprince002@gmail.com>
2026-04-02 01:48:17 -0400

    docs: add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+ const weatherForm = document.querySelector(".weatherForm");
+
+ const cityInput = document.querySelector(".cityInput");
+
+ const card = document.querySelector(".card");
+
+ const apikey = "ab862786b24840ad27e43a998d42902e"
+
+ weatherForm.addEventListener("submit" , event => {
+
+ });
+
+ async function getWeather(city){
+
+ }
+
+ fucntion displayWeatherInfo(data){
+
+ }
+
+ function displayError(message){
+    
+ }
